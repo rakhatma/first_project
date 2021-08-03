8404d52a8f222ae16e9822ee48c97b65cdba6657
Rakhat <amirov.kz@mail.ru>
2021-08-03 19:23:31 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Some text alert');
+// const result = confirm('Are you here?');
+
+// const answer = prompt('How old are you?', "");
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Whats your name?', "");
+answers[1] = prompt('How old are you?', "");
+answers[2] = prompt('Whats your last name?', "");
+
+
+
+
